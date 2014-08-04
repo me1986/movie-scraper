e6659464fdb71ec5b5c637666b55ba2f9d690c6b
Walter Schweitzer <wschweitzer00@gmail.com>
2014-08-04 18:10:17 -0400

    added info.sql

---
+++ info.sql
@@ -0,0 +1,8 @@
+SELECT COUNT(*)
+FROM sales_table
+WHERE (sales_date BETWEEN '2014-06-01 00:00:00' AND '2014-06-05 23:59:59')
+
+SELECT DISTINCT c.customer_first_name, c.customer_last_name,
+FROM customer_table c, sales_table s
+WHERE s.sales_amount > 200;
+
